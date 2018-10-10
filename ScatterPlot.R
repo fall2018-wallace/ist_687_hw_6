a15ae28c541d332283706146e2ed3c5eed2f0a88
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 20:52:21 -0400

    Saving ScatterPlot.R

--- ScatterPlot.R
+++ ScatterPlot.R
@@ -2,4 +2,4 @@
 library(ggplot2)
 
 #Generate Scatter Plot
-ggplot(Mstates, aes(x=population, y=percentOver18)) + geom_point(aes(size=Murder, color=Murder))
+Scater <- ggplot(Mstates, aes(x=population, y=percentOver18)) + geom_point(aes(size=Murder, color=Murder))
