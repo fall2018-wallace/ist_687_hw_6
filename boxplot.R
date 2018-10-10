1373fbd9c20e663974db69d32defcb5c40ab2f69
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 20:36:32 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -2,5 +2,5 @@
 #install.packages("ggplot2")
 library(ggplot2)
 #Creating a boxplot for population and murder rate
-ggplot(Mstates, aes(x=factor(1),population)) + geom_boxplot()
-ggplot(Mstates, aes(x=factor(1), Murder)) + geom_boxplot()
+PopBoxPlot <- ggplot(Mstates, aes(x=factor(1),population)) + geom_boxplot()
+MurderBoxPlot <- ggplot(Mstates, aes(x=factor(1), Murder)) + geom_boxplot()
