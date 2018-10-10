3f81c9ff4010cc63866afa27094ff16766d2e1d5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 20:12:49 -0400

    Saving dfStates.R

--- dfStates.R
+++ dfStates.R
@@ -1,1 +1,5 @@
 
+#read in the census data from HW #3
+dfStates <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"))
+dfStates <- dfStates[,-1:-4]
+dfStates <- dfStates[-c(1,53,10),]
