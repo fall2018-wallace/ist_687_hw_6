bdc24edb44a28db4c66e88a3fba4508a571e3322
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 20:42:31 -0400

    Saving Barchart.R

--- Barchart.R
+++ Barchart.R
@@ -1,1 +1,7 @@
 
+library(ggplot2)
+
+#Generate a Barchart for murders per state, rotate x-axis names, name the chart and reorder the states by murder rate,
+ggplot(Mstates, aes(y=MurderState, x= stateName)) + geom_col() +theme(axis.text.x = element_text(angle = 90, hjust=1))+ ggtitle("Total Murders")
+ggplot(Mstates, aes(y=MurderState, x= reorder(stateName, MurderState))) + geom_col() +theme(axis.text.x = element_text(angle = 90, hjust=1))+ ggtitle("Total Murders")
+ggplot(Mstates, aes(y=MurderState, x= reorder(stateName, MurderState))) + geom_col(aes(fill=percentOver18)) +theme(axis.text.x = element_text(angle = 90, hjust=1))+ ggtitle("Total Murders")
