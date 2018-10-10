3561cf387ca417a2393053b42579fadf3f0e4e39
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 20:29:24 -0400

    Saving Histogram.R

--- Histogram.R
+++ Histogram.R
@@ -1,1 +1,3 @@
 
+PopHist<-ggplot(Mstates, aes(x=population)) + geom_histogram(bins=30, color="black", fill="blue")
+MurderHist <- ggplot(Mstates, aes(x=Murder)) + geom_histogram(bins = 30, color="red", fill="black")
