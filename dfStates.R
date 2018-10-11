8ebfb21ca7a16952a5621e820e8d2d45eeb219fe
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:35:58 -0400

    Saving dfStates.R

--- dfStates.R
+++ dfStates.R
@@ -3,7 +3,5 @@
 dfStates <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"))
 dfStates <- dfStates[,-1:-4]
 dfStates <- dfStates[-c(1,53,10),]
-
-#Renaming all the columns
 colnames(dfStates) <- c("stateName","population","popOver18","percentOver18")
 
