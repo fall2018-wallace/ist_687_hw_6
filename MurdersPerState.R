93d7297a9620be6faea1e3440397cf15eb890cb8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:40:39 -0400

    Saving MurdersPerState.R

--- MurdersPerState.R
+++ MurdersPerState.R
@@ -1,4 +1,4 @@
 
 #Calucalting the number of murders per state
-MurderState <- (Mstates$population/Mstates$Murder)/100000
-str(MurderState)
+MurderState <- (Mstates$population*Mstates$Murder)/100000
+MurderState
