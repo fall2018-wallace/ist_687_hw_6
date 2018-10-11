ed7cba97e7375cb8652dd9b8dedfba9c139e9ee7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:47:05 -0400

    Saving dfStates.R

--- dfStates.R
+++ dfStates.R
@@ -2,7 +2,7 @@
 #read in the census data from HW #3
 dfStates <- read.csv("scprc_est_2017_18_pop_res.csv")
 dfStates <- dfStates[,-1:-4]
-View(dfStates)
+dfStates
 dfStates <- dfStates[-c(1,53,10),]
 colnames(dfStates) <- c("stateName","population","popOver18","percentOver18")
 
