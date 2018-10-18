ed94014037918f5937eadd68f334ab33900bcf92
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:48:31 -0400

    Saving USPop.R

--- USPop.R
+++ USPop.R
@@ -1,4 +1,8 @@
 
+library(ggplot2)
+#install.packages("ggmap")
+library(ggmap)
+
 #Map showing population per state
 USPop <- ggplot(Mstates, aes(map_id = stateName)) + geom_map(map = us, aes(fill=Murder),color="black")+ expand_limits(x = us$long, y = us$lat)+geom_point(aes(x=Mstates$Longitude, y=Mstates$Latitude, size = Mstates$population), color="red")+coord_map()
 USPop
