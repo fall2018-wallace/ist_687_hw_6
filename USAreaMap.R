e7deddc5a72dfab4546cac17e2e7adbc232e8520
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:43:57 -0400

    Saving USAreaMap.R

--- USAreaMap.R
+++ USAreaMap.R
@@ -4,7 +4,5 @@ library(ggplot2)
 library(ggmap)
 
 #color coded map based on area
-Mstates$stateName <- tolower(Mstates$stateName)
-us <- map_data("state")
 USmap <- ggplot(Mstates, aes(map_id = stateName)) + geom_map(map = us, aes(fill=Area), color="black")+ expand_limits(x = us$long, y = us$lat)+coord_map()
 USmap
