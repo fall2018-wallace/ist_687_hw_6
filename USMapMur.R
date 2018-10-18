96efc08f3b4efdb72335e49292f457ac5ca4a9cd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:41:38 -0400

    Saving USMapMur.R

--- USMapMur.R
+++ USMapMur.R
@@ -1,4 +1,8 @@
 
+library(ggplot2)
+#install.packages("ggmap")
+library(ggmap)
+
 #color shaded map based on murder rate of each state
 USMapMur <- ggplot(Mstates, aes(map_id = stateName)) + geom_map(map = us, aes(fill=Murder), color="black")+ expand_limits(x = us$long, y = us$lat)+coord_map()
 USMapMur
