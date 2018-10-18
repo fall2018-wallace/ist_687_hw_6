2cf07cd68f7f95794efccf13ab1878c407849255
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:23:57 -0400

    Saving MStates.R

--- MStates.R
+++ MStates.R
@@ -8,3 +8,9 @@ library(ggplot2)
 
 #used to get rid of the scientific notation, easier to understand in my opinion
 options(scipen = 999) 
+
+#create a column for the state area
+Mstates$Area <- state.area  
+state.center1<-data.frame(state.center)
+Mstates$Latitude <- state.center1$y
+Mstates$Longitude <- state.center1$x
